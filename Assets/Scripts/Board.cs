9e26c2e787ee1fd6f6c0a5c86b407e68851bb85d
hyeon11ok <hyeonsusin05@gmail.com>
2025-04-09 22:05:56 +0900

    Feat : 교차점 인덱스 계산 기능 및 사과 배치 미리 보기 기능 추가
    - x, y좌표를 기반으로 오목 판 위의 교차점 인덱스 반환
    - 마우스 좌표 받아와 오목 판 위에 있는 경우에만 인덱스 계산하여 반환
    - 계산한 인덱스를 활용해 해당 인덱스의 사과  활성화하여 클릭시 배치될 사과 미리 보기

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -17,6 +17,9 @@ public class Board : MonoBehaviour
     private Color red = new Color(221 / 255, 41 / 255, 58 / 255);
     private Color green = new Color(169 / 255, 200 / 255, 16 / 255);
 
+    int lastIdx = -1;
+    GameObject lastApple;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,28 +37,69 @@ public class Board : MonoBehaviour
             tmp.SetActive(false);
             apples.Add(tmp);
         }
+
+        Debug.Log(IndexToPosition(-4.5f, 4f));
+        Debug.Log(IndexToPosition(4.5f, 4.5f));
+        Debug.Log(IndexToPosition(4.4f, 4.5f));
+        // Debug.Log(9.1f % 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 마우스의 위치 좌표
+        Vector2 mPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+        // 마우스가 오목 판 안에 있는 경우
+        if(-4.75f < mPos.x &&  mPos.x < 4.75f &&
+            -4.75f < mPos.y && mPos.y < 4.75f) {
+            int idx = IndexToPosition(mPos.x, mPos.y); // 현재 마우스의 좌표와 인접한 인덱스 저장
+
+            // 새로운 인덱스라면 기존에 켜둔 사과를 꺼줘야함
+            if (idx != lastIdx) {
+                if(lastApple != null)
+                    lastApple.SetActive(false);
+
+                lastIdx = idx; // 마지막 인덱스 저장
+                apples[idx].SetActive(true); // 현재 인덱스의 사과 켜줌
+                lastApple = apples[idx]; // 마지막으로 켜준 사과 저장
+            }
+        }
+        else { // 마우스가 오목 판 밖으로 벗어나면 초기화
+            lastIdx = -1;
+            if(lastApple != null) {
+                lastApple.SetActive(false);
+                lastApple = null;
+            }
+        }
     }
 
     /// <summary>
     /// 좌표값을 오목 판의 인덱스 값으로 반환
+    /// 받아올 좌표는 
+    /// -4.75 < x && x < 4.75
+    /// -4.75 < y && y < 4.75
     /// </summary>
     /// <param name="x">x좌표</param>
     /// <param name="y">y좌표</param>
     private int IndexToPosition(float x, float y) {
         // 시작점부터 지정 좌표까지의 거리
-        float xDist = x - (xOffset - (size / 2)); 
-        float yDist = (yOffset - (size / 2)) - y;
+        float xDist = x - (xOffset); 
+        float yDist = (yOffset) - y;
 
         // x, y 각각 몇 번째 칸에 있는지 계산
         int xCnt = (int)(xDist / size);
         int yCnt = (int)(yDist / size);
 
-        return (yCnt - 1) * 19 + xCnt - 1;
+        // 사과는 칸이 아닌 교차점에 존재하기 때문에
+        // 나머지값을 0.25기준으로 비교하여 칸수를 1더할지 판단
+        if(xDist % size > 0.25f) {
+            xCnt++;
+        }
+        if(yDist % size > 0.25f) {
+            yCnt++;
+        }
+
+        return yCnt * 19 + xCnt;
     }
 }
